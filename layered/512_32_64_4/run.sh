252972ac4600e4b2fc63b5d1be7a2374c2c7fbbd
Seiya Tsukamoto <seiya.tsukamoto@ldas-pcdev2.ligo.caltech.edu>
2025-09-04 12:02:20 -0700

    added model

--- layered/512_32_64_4/run.sh
+++ layered/512_32_64_4/run.sh
@@ -7,4 +7,4 @@ export AFRAME_CONDOR_DIR=/home/seiya.tsukamoto/aframe/layered/512_32_64_4/condor
 export AFRAME_RESULTS_DIR=/home/seiya.tsukamoto/aframe/layered/512_32_64_4/results
 export AFRAME_TMPDIR=/home/seiya.tsukamoto/aframe/layered/512_32_64_4/tmp/
 
-LAW_CONFIG_FILE=/home/seiya.tsukamoto/aframe/layered/512_32_64_4/sandbox.cfg uv run --directory /home/seiya.tsukamoto/aframe law run aframe.pipelines.sandbox.Sandbox --workers 10 --gpus 0,1,2,3,4,5,6,7 --dev
+LAW_CONFIG_FILE=/home/seiya.tsukamoto/aframe/layered/512_32_64_4/sandbox.cfg uv run --directory /home/seiya.tsukamoto/aframe law run aframe.pipelines.sandbox.Sandbox --workers 8 --gpus 0,1,2,3 --dev
