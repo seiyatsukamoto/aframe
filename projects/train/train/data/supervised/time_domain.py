4dfbfc952eac1da0cc3b49f8b204f894aca8776b
Seiya Tsukamoto <seiya.tsukamoto@ldas-pcdev6.ligo-wa.caltech.edu>
2026-07-02 16:03:39 -0700

    added topk

--- projects/train/train/data/supervised/time_domain.py
+++ projects/train/train/data/supervised/time_domain.py
@@ -4,7 +4,8 @@ from typing import Literal
 
 from train.data.supervised.supervised import SupervisedAframeDataset
 #from ml4gw.transforms import Heterodyne
-from ml4gw.transforms import Heterodyne_fromdir as Heterodyne
+#from ml4gw.transforms import Heterodyne_from_dir as Heterodyne
+from ml4gw.transforms.heterodyne_from_file import Heterodyne_from_file as Heterodyne
 
 import numpy as np
 import torch.nn.functional as F
@@ -88,7 +89,7 @@ class HeterodyneTimeDomainSupervisedAframeDataset(SupervisedAframeDataset):
         self.heterodyne_transform = Heterodyne(
             sample_rate=self.hparams.sample_rate,
             kernel_length=self.hparams.kernel_length,
-            phase_dir=self.phase_dir,
+            chirp_mass_file=self.phase_dir,
             return_type="time",
         )
 
@@ -183,22 +184,27 @@ class TopKHeterodyneTimeDomainSupervisedAframeDataset(SupervisedAframeDataset):
     def build_val_batches(self, background, signals):
         X_bg, X_inj, psds = super().build_val_batches(background, signals)
         X_bg = self.whitener(X_bg, psds)
+        X_bg_td = X_bg.detach().clone()
         X_bg = self.heterodyne_transform(X_bg)
         _B_bg, _C_bg, _M_bg, _T_bg = X_bg.shape
         X_bg = X_bg.reshape(_B_bg, _C_bg * _M_bg, _T_bg)
         X_bg = self.topk_bin(X_bg, _M_bg, _B_bg)
+        X_bg = torch.cat([X_bg_td, X_bg], dim=1)
         # whiten each view of injections
         X_fg = []
+        X_fg_td = []
         for inj in X_inj:
             inj = self.whitener(inj, psds)
+            X_fg_td.append(inj.detach().clone())
             inj = self.heterodyne_transform(inj)
             X_fg.append(inj)
         X_fg = torch.stack(X_fg)
+        X_fg_td = torch.stack(X_fg_td)
         _V_fg, _B_fg, _C_fg, _M_fg, _T_fg = X_fg.shape
         X_fg = X_fg.view(_V_fg*_B_fg, _C_fg * _M_fg, _T_fg)
         X_fg = self.topk_bin(X_fg, _M_fg, _V_fg*_B_fg)
         X_fg = X_fg.view(_V_fg, _B_fg, _C_fg * self.k, _T_fg)
-
+        X_fg = torch.cat([X_fg_td, X_fg], dim=2)
         if self.keep_last_n_seconds is not None:
             return X_bg[..., -self.keep_last_n_samples :].float(), X_fg[
                 ..., -self.keep_last_n_samples :
@@ -209,14 +215,21 @@ class TopKHeterodyneTimeDomainSupervisedAframeDataset(SupervisedAframeDataset):
     def inject(self, X, waveforms=None):
         X, y, psds = super().inject(X, waveforms)
         X = self.whitener(X, psds)
+        if self.keep_last_n_seconds is not None:
+            X_td = X[..., -self.keep_last_n_samples :].float().detach().clone()
+        else:
+            X_td = X.float().detach().clone()
+        
         X = self.heterodyne_transform(X)
         _B, _C, _M, _T = X.shape
         X = X.view(_B, _C * _M, _T)
         X = self.topk_bin(X, _M, _B)
         if self.keep_last_n_seconds is not None:
-            return X[..., -self.keep_last_n_samples :].float(), y
+            X = X[..., -self.keep_last_n_samples :].float()
         else:
-            return X.float(), y
+            X = X.float()
+        
+        return torch.cat([X_td, X], dim=1), y
 
     def topk_bin(self, X, _M, _B):
         pooled = F.avg_pool1d(X.abs(), kernel_size = 31, stride = 5, padding = 0)
